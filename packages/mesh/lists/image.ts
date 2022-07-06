0b45eb0841f1a4bbf2e65697ffe8cb484b55ef50
hcchien <hcchien@gmail.com>
2022-07-06 18:33:01 +0800

    fix(mesh): update the image uploading with fs mounting

--- packages/mesh/lists/image.ts
+++ packages/mesh/lists/image.ts
@@ -1,12 +1,7 @@
 import { list } from '@keystone-6/core'
-import { text } from '@keystone-6/core/fields'
+import { image, text } from '@keystone-6/core/fields'
 import { customFields, utils } from '@mirrormedia/lilith-core'
 const { allowRoles, admin, moderator, editor } = utils.accessControl
-
-import { CustomFile } from '../../customFields'
-import { ImageFileAdapter } from '../../customFields/CustomFile/ImageFileAdapter'
-
-const imageFileAdapter = new ImageFileAdapter('image')
 
 const listConfigurations = list({
   db: {
@@ -17,15 +12,82 @@ const listConfigurations = list({
       label: '標題',
       validation: { isRequired: true },
     }),
-    file: CustomFile({
-      label: '檔案（建議長邊大於 2000 pixel）',
-      customConfig: {
-        fileType: 'image',
+    file: image(),
+    resized: virtual({
+      field: graphql.field({
+        type: graphql.object<{
+          original: string
+          w480: string
+          w800: string
+          w1200: string
+          w1600: string
+          w2400: string
+        }>()({
+          name: 'ResizedImages',
+          fields: {
+            original: graphql.field({ type: graphql.String }),
+            w480: graphql.field({ type: graphql.String }),
+            w800: graphql.field({ type: graphql.String }),
+            w1200: graphql.field({ type: graphql.String }),
+            w1600: graphql.field({ type: graphql.String }),
+            w2400: graphql.field({ type: graphql.String }),
+          },
+        }),
+        resolve(item: Record<string, unknown>) {
+          const empty = {
+            original: '',
+            w480: '',
+            w800: '',
+            w1200: '',
+            w1600: '',
+            w2400: '',
+          }
+
+          // For backward compatibility,
+          // this image item is uploaded via `GCSFile` custom field.
+          if (item?.urlOriginal) {
+            return Object.assign(empty, {
+              original: item.urlOriginal,
+            })
+          }
+
+          const rtn : Record<string, string> = {}
+          const filename = item?.imageFile_id
+
+          if (!filename) {
+            return empty
+          }
+
+          const extension = item?.imageFile_extension
+            ? '.' + item.imageFile_extension
+            : ''
+          const width = typeof item?.imageFile_width === 'number' ? item.imageFile_width : 0
+          const height = typeof item?.imageFile_height === 'number' ? item.imageFile_height : 0
+
+          const resizedTargets =
+            width >= height
+              ? ['w480', 'w800', 'w1600', 'w2400']
+              : ['w480', 'w800', 'w1200', 'w1600']
+
+          resizedTargets.forEach((target) => {
+            rtn[
+              target
+            ] = `${config.googleCloudStorage.origin}/${config.googleCloudStorage.bucket}/images/${filename}-${target}${extension}`
+          })
+
+          rtn[
+            'original'
+          ] = `${config.googleCloudStorage.origin}/${config.googleCloudStorage.bucket}/images/${filename}${extension}`
+          return Object.assign(empty, rtn)
+        },
+      }),
+      ui: {
+        query: '{ original w480 w800 w1200 w1600 w2400 }',
       },
     }),
     urlOriginal: text({
       ui: {
-        // createView: { fieldMode: 'hidden' },
+        createView: { fieldMode: 'hidden' },
         // itemView: { fieldMode: 'read' },
       },
     }),
