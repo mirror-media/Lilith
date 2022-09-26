5967d20073354f49643602cea8c123e43918f05b
hcchien <hcchien@gmail.com>
2022-09-26 15:26:59 +0800

    feat: reset the schema to apply the existing schema and data

--- packages/openrelationship/lists/OrganizationsRelationship.ts
+++ packages/openrelationship/lists/OrganizationsRelationship.ts
@@ -22,15 +22,55 @@ const listConfigurations = list ({
       many: false,
       ref: 'Organization',
     }),
-    relative: text({ label: '組織與關係組織的關係' }),
-    start_date_year: integer({ label: '開始年' }),
-    start_date_month: integer({ label: '開始月' }),
-    start_date_day: integer({ label: '開始日' }),
+    relative: text({ 
+      label: '組織與關係組織的關係',
+	  db: {
+		isNullable: true,
+	  },
+    }),
+    start_date_year: integer({ 
+      label: '開始年',
+	  db: {
+		isNullable: true,
+	  },
+    }),
+    start_date_month: integer({ 
+      label: '開始月',
+	  db: {
+		isNullable: true,
+	  },
+    }),
+    start_date_day: integer({ 
+      label: '開始日',
+	  db: {
+		isNullable: true,
+	  },
+    }),
 
-    end_date_year: integer({ label: '結束年' }),
-    end_date_month: integer({ label: '結束月' }),
-    end_date_day: integer({ label: '結束日' }),
-    source: text({ label: '資料來源' }),
+    end_date_year: integer({ 
+      label: '結束年',
+	  db: {
+		isNullable: true,
+	  },
+    }),
+    end_date_month: integer({ 
+      label: '結束月',
+	  db: {
+		isNullable: true,
+	  },
+    }),
+    end_date_day: integer({ 
+      label: '結束日',
+	  db: {
+		isNullable: true,
+	  },
+    }),
+    source: text({ 
+      label: '資料來源',
+	  db: {
+		isNullable: true,
+	  },
+    }),
   },
   access: {
 	operation: {
