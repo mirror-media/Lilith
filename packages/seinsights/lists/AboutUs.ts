c0f740bec2a8ba0ca90d3bbd305ca00ea9fdb74a
hcchien <hcchien@gmail.com>
2023-01-10 15:21:59 +0800

    feat: update the about us field type from draftjs to textarea

--- packages/seinsights/lists/AboutUs.ts
+++ packages/seinsights/lists/AboutUs.ts
@@ -1,22 +1,15 @@
-import { customFields, utils } from '@mirrormedia/lilith-core'
+import { utils } from '@mirrormedia/lilith-core'
 import { list } from '@keystone-6/core'
-import { json } from '@keystone-6/core/fields'
+import { text } from '@keystone-6/core/fields'
 
 const { allowRoles, admin, moderator, editor } = utils.accessControl
 
-
 const listConfigurations = list({
   fields: {
-    aboutUs: customFields.richTextEditor({
+    aboutUs: text({
       label: 'about us',
-      disabledButtons: ['header-four', 'code', 'code-block', 'annotation', 'info-box'],
-    }),
-    
-    apiData: json({
-      label: '資料庫使用',
       ui: {
-        createView: { fieldMode: 'hidden' },
-        itemView: { fieldMode: 'read' },
+        displayMode: 'textarea',
       },
     }),
   },
@@ -32,17 +25,6 @@ const listConfigurations = list({
       update: allowRoles(admin, moderator, editor),
       create: allowRoles(admin, moderator, editor),
       delete: allowRoles(admin),
-    },
-  },
-  hooks: {
-    resolveInput: async ({ resolvedData }) => {
-      const { aboutUs } = resolvedData
-      if (aboutUs) {
-        resolvedData.apiData = customFields.draftConverter
-          .convertToApiData(aboutUs)
-          .toJS()
-      }
-      return resolvedData
     },
   },
 })
