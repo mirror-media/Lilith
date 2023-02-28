013a639004f34dd5ec52e8c5db20bed5e13714ba
Tsuki Akiba <erase2004@gmail.com>
2023-02-28 14:12:52 +0800

    refactor(readr): fix ts errors, field errors and code format in EditorChoice

--- packages/readr/lists/EditorChoice.ts
+++ packages/readr/lists/EditorChoice.ts
@@ -1,62 +1,62 @@
+// @ts-ignore: no definition
 import { utils } from '@mirrormedia/lilith-core'
-import { list } from '@keystone-6/core';
-import { relationship, integer, select, text, timestamp } from '@keystone-6/core/fields';
-	  
-const {
-  allowRoles,
-  admin,
-  moderator,
-  editor,
-  owner,
-} = utils.accessControl
+import { list } from '@keystone-6/core'
+import {
+  relationship,
+  integer,
+  select,
+  text,
+  timestamp,
+} from '@keystone-6/core/fields'
 
-const listConfigurations = list ({
+const { allowRoles, admin, moderator, editor } = utils.accessControl
+
+const listConfigurations = list({
   fields: {
-	sortOrder: integer({
-	  label: '排序順位',
-	}),
-	name: text({
-	  label: '標題',
-	}),
-	description: text({
-	  label: '描述',
-	}),
-	choices: relationship({
-      ref: 'Post', 
-      many: 'false', 
-      label: '精選文章' 
+    sortOrder: integer({
+      label: '排序順位',
     }),
-	link: text({
-	  label: '連結',
-	}),
-	heroImage: relationship({
-	  label: '首圖',
-	  ref: 'Photo',
-	}),
+    name: text({
+      label: '標題',
+    }),
+    description: text({
+      label: '描述',
+    }),
+    choices: relationship({
+      ref: 'Post',
+      many: false,
+      label: '精選文章',
+    }),
+    link: text({
+      label: '連結',
+    }),
+    heroImage: relationship({
+      label: '首圖',
+      ref: 'Photo',
+    }),
     state: select({
-      defaultValue: 'draft', 
-      options: [ 
-        {label: 'draft', value: 'draft'}, 
-        {label: 'published', value: 'published'}, 
-        {label: 'scheduled', value: 'scheduled'}, 
-        {label: 'archived', value: 'archived'}, 
-        {label: 'invisible', value: 'invisible'}
-      ], 
-      isIndexed: true, 
-      label: '狀態' 
+      defaultValue: 'draft',
+      options: [
+        { label: 'draft', value: 'draft' },
+        { label: 'published', value: 'published' },
+        { label: 'scheduled', value: 'scheduled' },
+        { label: 'archived', value: 'archived' },
+        { label: 'invisible', value: 'invisible' },
+      ],
+      isIndexed: true,
+      label: '狀態',
     }),
-	publishTime: timestamp({
-	  label: '發布時間',
-	}),
-
+    publishTime: timestamp({
+      label: '發布時間',
+    }),
   },
   access: {
-	operation: {
-	  query: allowRoles(admin, moderator, editor),
-	  update: allowRoles(admin, moderator),
-	  create: allowRoles(admin, moderator),
-	  delete: allowRoles(admin),
-	},
+    operation: {
+      query: allowRoles(admin, moderator, editor),
+      update: allowRoles(admin, moderator),
+      create: allowRoles(admin, moderator),
+      delete: allowRoles(admin),
+    },
   },
 })
 export default utils.addTrackingFields(listConfigurations)
