f9482ebc2c313ea8bfffa8344258bf52922948db
nickhsine <nickhsine97753017@gmail.com>
2023-03-08 17:48:52 +0800

    fix(editools): correct draco libs path

--- packages/editools/public/three/utils.js
+++ packages/editools/public/three/utils.js
@@ -7,7 +7,7 @@ import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js'
  * Load model
  */
 const dracoLoader = new DRACOLoader()
-dracoLoader.setDecoderPath('https://cdn.skypack.dev/three@0.149.0/examples/js/libs/draco/')
+dracoLoader.setDecoderPath('https://cdn.skypack.dev/three@0.149.0/examples/jsm/libs/draco/')
 const gltfLoader = new GLTFLoader()
 gltfLoader.setDRACOLoader(dracoLoader)
 
