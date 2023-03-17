4cc257ca1fe03114f7a7a166fea03f0aea973755
Xuan <104489150+ChangRongXuan@users.noreply.github.com>
2023-03-17 14:04:05 +0800

    feat(draft-renderer): adjust `READr` block style

--- packages/draft-renderer/src/website/readr/block-renderers/image-block.tsx
+++ packages/draft-renderer/src/website/readr/block-renderers/image-block.tsx
@@ -2,16 +2,29 @@ import React from 'react'
 import styled from 'styled-components'
 import { DraftEntityInstance } from 'draft-js'
 
+const Figure = styled.figure`
+  width: calc(100% + 40px);
+  transform: translateX(-20px);
+`
+
 const Image = styled.img`
   width: 100%;
 `
 
-const Figure = styled.figure`
-  margin-block: unset;
-  margin-inline: unset;
-  margin: 0 10px;
-`
+const FigureCaption = styled.figcaption`
+  width: 100%;
+  font-size: 14px;
+  line-height: 21px;
+  text-align: justify;
+  color: rgba(0, 9, 40, 0.5);
+  padding: 0 20px;
+  margin: 8px 0 0;
 
+  ${({ theme }) => theme.breakpoint.xl} {
+    font-size: 16px;
+    line-height: 24px;
+  }
+`
 const Anchor = styled.a`
   text-decoration: none;
 `
@@ -25,7 +38,7 @@ export function ImageBlock(entity: DraftEntityInstance) {
         src={resized?.original}
         onError={(e) => (e.currentTarget.src = imageFile?.url)}
       />
-      <figcaption>{desc}</figcaption>
+      <FigureCaption>{desc}</FigureCaption>
     </Figure>
   )
 
--- packages/draft-renderer/src/website/readr/block-renderers/info-box-block.tsx
+++ packages/draft-renderer/src/website/readr/block-renderers/info-box-block.tsx
@@ -3,9 +3,29 @@ import { ContentBlock, ContentState } from 'draft-js'
 import styled from 'styled-components'
 
 const InfoBoxRenderWrapper = styled.div`
-  background-color: #f5f4f3;
-  padding: 30px;
+  background: #f6f6fb;
   position: relative;
+  padding: 24px 0;
+`
+
+const InfoTitle = styled.div`
+  width: 100%;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 150%;
+  letter-spacing: 0.03em;
+  color: #000928;
+  border-left: 8px solid #04295e;
+  padding: 0 32px 0 24px;
+  margin-bottom: 8px;
+`
+
+const InfoContent = styled.div`
+  padding: 0px 32px;
+  font-size: 16px;
+  line-height: 160%;
+  color: rgba(0, 9, 40, 0.87);
 `
 
 type InfoBoxBlockProps = {
@@ -31,8 +51,10 @@ export function InfoBoxBlock(props: InfoBoxBlockProps) {
 
   return (
     <InfoBoxRenderWrapper>
-      <h2>{title}</h2>
-      <div dangerouslySetInnerHTML={{ __html: body }} />
+      <InfoTitle>{title}</InfoTitle>
+      <InfoContent>
+        <div dangerouslySetInnerHTML={{ __html: body }} />
+      </InfoContent>
     </InfoBoxRenderWrapper>
   )
 }
