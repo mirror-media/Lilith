87500973f86f4e176ecb03b9038f5e1b1d1b0c05
DianYangFu <dianyangfu@gmail.com>
2023-03-21 21:49:01 +0800

    style(draft-renderer/mirrormedia): add stylesheet of divider block

--- packages/draft-renderer/src/website/mirrormedia/block-renderers/divider-block.tsx
+++ packages/draft-renderer/src/website/mirrormedia/block-renderers/divider-block.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Divider = styled.hr`
-  box-sizing: border-box;
-  border-width: 1px;
-  border-style: inset;
+  /* box-sizing: border-box; */
+  border-top: 1px solid red;
+  /* border-style: inset; */
 `
 
 export const DividerBlock = () => {
