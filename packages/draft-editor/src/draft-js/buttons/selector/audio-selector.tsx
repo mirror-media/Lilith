7cceecaf28ee8d2f1f421c6fac9407cc3e29577e
caesar <caesar.lee@mirrormedia.mg>
2023-03-31 15:27:18 +0800

    refactor(draft-editor): remove console

--- packages/draft-editor/src/draft-js/buttons/selector/audio-selector.tsx
+++ packages/draft-editor/src/draft-js/buttons/selector/audio-selector.tsx
@@ -110,7 +110,6 @@ function AudioGrid(props: {
   onSelect: AudioEntityOnSelectFn
 }) {
   const { audio, onSelect, isSelected } = props
-  console.log('audio', audio)
   return (
     <AudioGridWrapper key={audio?.id} onClick={() => onSelect(audio)}>
       <AudioSelected>
