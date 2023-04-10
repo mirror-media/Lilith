aec1e68bcfb7b5f413872d4b30e87d7182e5ed5c
tingwei <yatiti84@gmail.com>
2023-04-10 13:50:37 +0800

    feat(editools): add relatedtopic in component_help

--- packages/editools/lists/ComponentHelp.ts
+++ packages/editools/lists/ComponentHelp.ts
@@ -19,6 +19,10 @@ const listConfigurations = list({
       label: '套件說明',
       ui: { displayMode: 'textarea' }
     }),
+    relatedTopics: text({
+      label: '使用到此套件的專題',
+      ui: { displayMode: 'textarea' }
+    }),
   },
   access: {
     operation: {
---
+++ packages/editools/migrations/20230410054950_add_related_topics_in_component_help/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "ComponentHelp" ADD COLUMN     "relatedTopics" TEXT NOT NULL DEFAULT E'';
