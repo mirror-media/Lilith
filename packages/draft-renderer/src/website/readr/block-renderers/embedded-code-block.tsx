337d8439a8e316988b9c00832316a72405bda359
caesar <caesar.lee@mirrormedia.mg>
2023-04-19 17:54:35 +0800

    refactor(draft-renderer/embed-code): optimize embed code UX
    - split scripts from embed code string and append them in useEffect

--- packages/draft-renderer/src/website/readr/block-renderers/embedded-code-block.tsx
+++ packages/draft-renderer/src/website/readr/block-renderers/embedded-code-block.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useMemo, useRef } from 'react'
 import { DraftEntityInstance } from 'draft-js'
 import styled from 'styled-components'
+import { parse } from 'node-html-parser'
 
 export const Block = styled.div`
   position: relative;
@@ -25,45 +26,62 @@ export const EmbeddedCodeBlock = (entity: DraftEntityInstance) => {
   const { caption, embeddedCode } = entity.getData()
   const embedded = useRef(null)
 
-  useEffect(() => {
-    if (!embedded.current) return
-    const node: HTMLElement = embedded.current
+  // const ele = parse(`<div id="draft-embed">${embeddedCode}</div>`)
 
-    const fragment = document.createDocumentFragment()
-
-    // `embeddedCode` is a string, which may includes
-    // multiple '<script>' tags and other html tags.
-    // For executing '<script>' tags on the browser,
-    // we need to extract '<script>' tags from `embeddedCode` string first.
-    //
-    // The approach we have here is to parse html string into elements,
-    // and we could use DOM element built-in functions,
-    // such as `querySelectorAll` method, to query '<script>' elements,
-    // and other non '<script>' elements.
-    const parser = new DOMParser()
-    const ele = parser.parseFromString(
-      `<div id="draft-embed">${embeddedCode}</div>`,
-      'text/html'
-    )
-    const scripts = ele.querySelectorAll('script')
-    const nonScripts = ele.querySelectorAll('div#draft-embed > :not(script)')
-
-    nonScripts.forEach((ele) => {
-      fragment.appendChild(ele)
-    })
-
-    scripts.forEach((s) => {
-      const scriptEle = document.createElement('script')
-      const attrs = s.attributes
-      for (let i = 0; i < attrs.length; i++) {
-        scriptEle.setAttribute(attrs[i].name, attrs[i].value)
-      }
-      scriptEle.text = s.text || ''
-      fragment.appendChild(scriptEle)
-    })
-
-    node.appendChild(fragment)
+  const ele = useMemo(() => {
+    return parse(`<div id="draft-embed">${embeddedCode}</div>`)
   }, [embeddedCode])
+  const nonScripts = useMemo(
+    () => ele.querySelectorAll('div#draft-embed > :not(script)'),
+    [ele]
+  )
+  const scripts = useMemo(() => ele.querySelectorAll('script'), [ele])
+  const nonScriptHtml = useMemo(
+    () => nonScripts.reduce((prev, next) => prev + next.toString(), ''),
+    [embeddedCode]
+  )
+
+  useEffect(() => {
+    if (embedded.current) {
+      const node: HTMLElement = embedded.current
+
+      const fragment = document.createDocumentFragment()
+
+      // `embeddedCode` is a string, which may includes
+      // multiple '<script>' tags and other html tags.
+      // For executing '<script>' tags on the browser,
+      // we need to extract '<script>' tags from `embeddedCode` string first.
+      //
+      // The approach we have here is to parse html string into elements,
+      // and we could use DOM element built-in functions,
+      // such as `querySelectorAll` method, to query '<script>' elements,
+      // and other non '<script>' elements.
+      // const parser = new DOMParser()
+      // const ele = parser.parseFromString(
+      //   `<div id="draft-embed">${embeddedCode}</div>`,
+      //   'text/html'
+      // )
+      // const scripts = ele.querySelectorAll('script')
+
+      scripts.forEach((s) => {
+        const scriptEle = document.createElement('script')
+        const attrs = s.attributes
+        for (const key in attrs) {
+          scriptEle.setAttribute(key, attrs[key])
+        }
+        scriptEle.text = s.text || ''
+        fragment.appendChild(scriptEle)
+      })
+
+      node.appendChild(fragment)
+
+      return () => {
+        node.querySelectorAll('script').forEach(function(script) {
+          node.removeChild(script)
+        })
+      }
+    }
+  }, [scripts])
 
   return (
     <div>
@@ -77,7 +95,12 @@ export const EmbeddedCodeBlock = (entity: DraftEntityInstance) => {
         // hijacking the users' cursors.
       }
       <input hidden disabled />
-      <Block ref={embedded} />
+      <Block
+        ref={embedded}
+        dangerouslySetInnerHTML={{
+          __html: nonScriptHtml,
+        }}
+      />
       {caption ? <Caption>{caption}</Caption> : null}
     </div>
   )
