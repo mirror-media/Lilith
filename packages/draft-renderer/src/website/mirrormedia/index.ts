3d5ef1df69705c2749fc1c962571b74b958a9e46
Dian-Yang Fu <85677992+dyfu95@users.noreply.github.com>
2023-04-20 22:07:44 +0800

    feat(draft-renderer): add utils function to check has content in raw content block (#310)
    * feat(draft-renderer): add utils function to check has content in raw content block
    
    * chore: bump verion into `1.1.0-alpha.19`

--- packages/draft-renderer/src/website/mirrormedia/index.ts
+++ packages/draft-renderer/src/website/mirrormedia/index.ts
@@ -1,11 +1,12 @@
 import { blockRenderers } from './block-renderers'
 import { entityDecorators } from './entity-decorators'
 import DraftRenderer from './draft-renderer'
-
+import { hasContentInRawContentBlock } from './utils'
 const MirrorMedia = {
   DraftRenderer,
   blockRenderers,
   entityDecorators,
+  hasContentInRawContentBlock,
 }
 
 export default MirrorMedia
