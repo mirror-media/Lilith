34023db22a8d7653d89594d76a3adb4a94849ec9
nickhsine <nickhsine97753017@gmail.com>
2023-05-12 10:57:13 +0800

    refactor(core)!: remove unused custom-fields

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,18 +1,12 @@
 import * as accessControl from './utils/accessControl'
 import addManualOrderRelationshipFields from './utils/manual-order-relationship'
-import { draftConverter } from '@mirrormedia/lilith-draft-editor'
-import { CustomFile } from './custom-fields/file'
-import { CustomRelationship } from './custom-fields/relationship'
-import { CustomTimestamp } from './custom-fields/timestamp'
 import { addTrackingFields } from './utils/trackingHandler'
+import { draftConverter } from '@mirrormedia/lilith-draft-editor'
 import { richTextEditor } from './custom-fields/rich-text-editor'
 
 export const customFields = {
-  file: CustomFile,
-  relationship: CustomRelationship,
-  timestamp: CustomTimestamp,
-  richTextEditor,
   draftConverter,
+  richTextEditor,
 }
 
 export const utils = {
