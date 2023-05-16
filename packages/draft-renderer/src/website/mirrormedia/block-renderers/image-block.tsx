2be214e9edfecfdda39d073f868c3394b8bfd3a7
DianYangFu <dianyangfu@gmail.com>
2023-05-17 02:08:28 +0800

    style(draft-renderer): add style for `contentLayout: premium`

--- packages/draft-renderer/src/website/mirrormedia/block-renderers/image-block.tsx
+++ packages/draft-renderer/src/website/mirrormedia/block-renderers/image-block.tsx
@@ -11,12 +11,24 @@ import {
   clearAllBodyScrollLocks,
 } from 'body-scroll-lock'
 
-const imageFigureLayoutNormal = css``
+const imageFigureLayoutNormal = css`
+  .readr-media-react-image {
+    width: 100%;
+  }
+`
 const imageFigureLayoutWide = css`
   .readr-media-react-image {
-    position: relative;
-    max-width: calc(100% + 20px + 20px);
-    transform: translateX(-20px);
+    width: 100vw;
+    max-width: 640px;
+    transform: translateX(min(50vw - 50% - 20px, 0px));
+  }
+`
+const imageFigureLayoutPremium = css`
+  .readr-media-react-image {
+    width: 100vw;
+    max-width: 640px;
+
+    transform: translateX(min(50vw - 50% - 20px, 0px));
   }
 `
 
@@ -34,15 +46,11 @@ const figcaptionLayoutWide = css`
   margin-top: 16px;
   position: relative;
   border-top: 1px solid rgba(0, 0, 0, 0.1);
-  ${({ theme }) => theme.breakpoint.md} {
-    max-width: calc(100% + 20px + 20px);
-    transform: translateX(-20px);
-  }
 `
 
 const Figure = styled.figure`
-  margin-block: unset;
-  margin-inline: unset;
+  /* margin-block: unset; */
+  /* margin-inline: unset; */
   ${defaultMarginTop}
   ${defaultMarginBottom}
   .readr-media-react-image {
@@ -56,6 +64,8 @@ const ImageFigure = styled(Figure)`
         return imageFigureLayoutNormal
       case 'wide':
         return imageFigureLayoutWide
+      case 'premium':
+        return imageFigureLayoutPremium
       default:
         return imageFigureLayoutNormal
     }
@@ -165,7 +175,7 @@ export function ImageBlock(
         images={resized}
         defaultImage={defaultImage}
         loadingImage={loadingImage}
-        width={'100vw'}
+        width={''}
         height={'auto'}
         objectFit={'contain'}
         alt={name}
