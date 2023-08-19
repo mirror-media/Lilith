ac152d8cae454dacd1fd4eac904e657aa6b62de8
YiHsuan <v61265@gmail.com>
2023-08-19 23:00:58 +0800

    refactor(draft-renderer): add layout of default amp-img

--- packages/draft-renderer/src/website/mirrormedia/block-renderers/image-block.tsx
+++ packages/draft-renderer/src/website/mirrormedia/block-renderers/image-block.tsx
@@ -207,7 +207,7 @@ export function ImageBlock(
   const imageJsx = isAmp ? (
     <AmpImgWrapper>
       <amp-img src={resized?.original} alt={name} layout="fill">
-        <amp-img src={defaultImage} alt={name}></amp-img>
+        <amp-img src={defaultImage} alt={name} layout="fill"></amp-img>
       </amp-img>
     </AmpImgWrapper>
   ) : (
