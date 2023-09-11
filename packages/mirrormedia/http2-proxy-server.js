7d1c1e1591428c351dd60c13f5e8e4eb1aebddc0
Tsuki Akiba <>
2023-09-11 17:14:38 +0800

    refactor(mirrormedia): modify redis connection test

--- packages/mirrormedia/http2-proxy-server.js
+++ packages/mirrormedia/http2-proxy-server.js
@@ -2,30 +2,6 @@
 const http2 = require('http2')
 const proxy = require('http2-proxy')
 const finalhandler = require('finalhandler')
-const Redis = require('ioredis')
-
-function createRedisInstance() {
-  return new Redis(process.env.REDIS_SERVER ?? '')
-}
-
-async function testRedisConnection() {
-  const testKey = 'testKey'
-  const testVal = '5'
-  try {
-    console.log('// create test redis instance //')
-    const redisInstance = createRedisInstance()
-
-    console.log('// write testKey to redis //')
-    await redisInstance.set(testKey, testVal)
-
-    console.log('// get testKey from redis //')
-    const value = await redisInstance.get(testKey)
-    console.log(`testVal: ${value}`)
-  } catch (err) {
-    console.log(err)
-  }
-}
-
 const keystoneServerPort = process.env.KEYSTONE_SERVER_PORT || '3000'
 const reverseProxyPort = process.env.REVERSE_PROXY_PORT || '3002'
 
@@ -48,5 +24,3 @@ server.on('request', (req, res) => {
 server.listen(reverseProxyPort, () => {
   console.log('server is listening on ' + reverseProxyPort)
 });
-
-testRedisConnection()
