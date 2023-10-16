2633df8d4051da7085495207e4a00d415fc262e7
hcchien <hcchien@gmail.com>
2023-10-16 14:27:11 +0800

    feat(openrelationship): update tag and politics schema

--- packages/openrelationship/lists/Election.ts
+++ packages/openrelationship/lists/Election.ts
@@ -61,11 +61,6 @@ const listConfigurations = list({
 		hidePoliticDetail: timestamp({
 		  label: '隱藏政見細節',
 	    }),
-        tags: relationship({
-          label: '標籤',
-          many: true,
-          ref: 'Tag',
-        }),
 		addComments: checkbox({
 		  label: '開放留言（心情）',
 		  defaultValue: false,
