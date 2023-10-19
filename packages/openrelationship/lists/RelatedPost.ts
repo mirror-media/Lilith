60de6a25a900c42598b90c8f2d062182736c1755
hcchien <hcchien@gmail.com>
2023-10-19 18:39:36 +0800

    feat(openrelationship): add new list called "RelatedPost" for partners reports

---
+++ packages/openrelationship/lists/RelatedPost.ts
@@ -0,0 +1,51 @@
+import { utils } from '@mirrormedia/lilith-core'
+import { list } from '@keystone-6/core';
+import {relationship,checkbox,select,text} from '@keystone-6/core/fields';
+	  
+const {
+  allowRoles,
+  admin,
+  moderator,
+  editor,
+  owner,
+} = utils.accessControl
+
+const listConfigurations = list ({
+  fields: {
+	name: text({
+      label: '文章標題', 
+      validation: { isRequired: true} 
+    }),
+    brief: text({
+      label: '前言',  
+      ui: { displayMode: 'textarea' } 
+    }),
+    url: text({
+      validation: { isRequired: true}, 
+      label: '文章網址' 
+    }),
+    ogIMage: text({
+      validation: { isRequired: true}, 
+      label: '文章 og 圖片',  
+    }),
+    partner: relationship({
+      label: '合作夥伴',
+	  ref: 'FactcheckPartner.posts',
+	  many: true,
+    }),
+    election: relationship({
+      label: '相關選舉',
+	  ref: 'Election',
+	  many: false,
+    }),
+  },
+  access: {
+	operation: {
+	  query: allowRoles(admin, moderator, editor),
+	  update: allowRoles(admin, moderator),
+	  create: allowRoles(admin, moderator),
+	  delete: allowRoles(admin),
+	},
+  },
+})
+export default utils.addTrackingFields(listConfigurations)
