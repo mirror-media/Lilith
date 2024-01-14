3fdd57530595537ecdbb44e903b43300a3e2bcd4
Xuan <as840922qq@gmail.com>
2024-01-14 22:08:25 +0800

    fix(readr): update migration.sql

--- packages/readr/migrations/20240114140754_add_is_featured_images_in_collaboration_migration_sql/migration.sql
+++ packages/readr/migrations/20240114140754_add_is_featured_images_in_collaboration_migration_sql/migration.sql
@@ -1,3 +1,9 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[name]` on the table `Project` will be added. If there are existing duplicate values, this will fail.
+
+*/
 -- AlterTable
 ALTER TABLE "Collaboration" ADD COLUMN     "ImageDesktop" INTEGER,
 ADD COLUMN     "ImageMobile" INTEGER,
@@ -12,6 +18,9 @@ CREATE INDEX "Collaboration_ImageTablet_idx" ON "Collaboration"("ImageTablet");
 
 -- CreateIndex
 CREATE INDEX "Collaboration_ImageDesktop_idx" ON "Collaboration"("ImageDesktop");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Project_name_key" ON "Project"("name");
 
 -- AddForeignKey
 ALTER TABLE "Collaboration" ADD CONSTRAINT "Collaboration_ImageMobile_fkey" FOREIGN KEY ("ImageMobile") REFERENCES "Image"("id") ON DELETE SET NULL ON UPDATE CASCADE;
