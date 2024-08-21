8f0206e41a43fd629f8def74ec5a9788e7e02c42
Tsuki Akiba <erase2004@gmail.com>
2024-08-21 13:05:58 +0800

    feat(mirrordaily): add Redux store

--- packages/mirrordaily/admin/pages/multi-images.tsx
+++ packages/mirrordaily/admin/pages/multi-images.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 import { PageContainer } from '@keystone-6/core/admin-ui/components'
 import { Heading } from '@keystone-ui/core'
+import { store } from '../redux/store'
+import { Provider } from 'react-redux'
 
 const H1 = styled.h1`
   color: red;
@@ -12,7 +14,9 @@ export default function MultiImages() {
       title="Multi Images"
       header={<Heading type="h3">Multi Images</Heading>}
     >
-      <H1>This is multi images</H1>
+      <Provider store={store}>
+        <H1>This is multi images</H1>
+      </Provider>
     </PageContainer>
   )
 }
