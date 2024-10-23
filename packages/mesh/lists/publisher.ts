c2390d9ceb82f4db6bd4654466c0dc6644390da0
hcchien <hcchien@gmail.com>
2024-10-23 11:21:57 +0800

    feat(mesh): add publish field into the user and type into the story

--- packages/mesh/lists/publisher.ts
+++ packages/mesh/lists/publisher.ts
@@ -1,23 +1,10 @@
 import { utils } from '@mirrormedia/lilith-core'
-import { list } from '@keystone-6/core';
-import {
-  text,
-  relationship,
-  password,
-  timestamp,
-  select,
-  checkbox,
-} from '@keystone-6/core/fields';
+import { list } from '@keystone-6/core'
+import { text, relationship, select, checkbox } from '@keystone-6/core/fields'
 
-const {
-  allowRoles,
-  admin,
-  moderator,
-  editor,
-  owner,
-} = utils.accessControl
+const { allowRoles, admin, moderator, editor } = utils.accessControl
 
-const listConfigurations = list ({
+const listConfigurations = list({
   fields: {
     title: text({ validation: { isRequired: false } }),
     official_site: text({ validation: { isRequired: true } }),
@@ -27,62 +14,67 @@ const listConfigurations = list ({
     description: text({ validation: { isRequired: false } }),
     customId: text({
       label: 'customId',
-	  validation: {
+      validation: {
         isRequired: true,
       },
-	    isindexed: 'unique',
+      isindexed: 'unique',
     }),
     sponsored: relationship({
       label: 'Sponsored',
       ref: 'Sponsorship.publisher',
       many: true,
     }),
-	lang: select({
-	  label: '語系',
-	  datatype: 'enum',
-	  options: [ 
-		{ label: '繁體中文', value: 'zh-TW' }, 
-		{ label: '英文', value: 'en-US' },
-		{ label: '日文', value: 'ja-Jp' }
+    lang: select({
+      label: '語系',
+      datatype: 'enum',
+      options: [
+        { label: '繁體中文', value: 'zh-TW' },
+        { label: '英文', value: 'en-US' },
+        { label: '日文', value: 'ja-Jp' },
       ],
-	  defaultValue: 'zh-TW',
-	}),
-	full_content: checkbox({
-	  defaultValue: false,
-	}),
-	full_screen_ad: select({
-	  label: '蓋板',
-	  datatype: 'enum',
-	  options: [ 
-		{ label: '手機', value: 'mobile' }, 
-		{ label: '桌機', value: 'desktop' },
-		{ label: '所有尺寸', value: 'all' },
-		{ label: '無', value: 'none' }
+      defaultValue: 'zh-TW',
+    }),
+    full_content: checkbox({
+      defaultValue: false,
+    }),
+    full_screen_ad: select({
+      label: '蓋板',
+      datatype: 'enum',
+      options: [
+        { label: '手機', value: 'mobile' },
+        { label: '桌機', value: 'desktop' },
+        { label: '所有尺寸', value: 'all' },
+        { label: '無', value: 'none' },
       ],
-	  defaultValue: 'none',
-	}),
-	source_type: select({
-	  label: '資料來源',
-	  datatype: 'enum',
-	  options: [ 
-		{ label: '鏡週刊', value: 'mirrormedia' }, 
-		{ label: 'READr', value: 'readr' },
-		{ label: '鏡週刊 external', value: 'mm_external' },
-		{ label: 'rss', value: 'rss' },
-    { label: 'empty', value: 'empty'}
+      defaultValue: 'none',
+    }),
+    source_type: select({
+      label: '資料來源',
+      datatype: 'enum',
+      options: [
+        { label: '鏡週刊', value: 'mirrormedia' },
+        { label: 'READr', value: 'readr' },
+        { label: '鏡週刊 external', value: 'mm_external' },
+        { label: 'rss', value: 'rss' },
+        { label: 'empty', value: 'empty' },
       ],
-	  defaultValue: 'empty',
-	}),
+      defaultValue: 'empty',
+    }),
     paywall: checkbox({
       defaultValue: false,
     }),
-	follower: relationship({
-	  ref: 'Member.follow_publisher',
-	  many: true,
-	}),
-      //posts: relationship({ ref: 'Post.author', many: true }),
+    follower: relationship({
+      ref: 'Member.follow_publisher',
+      many: true,
+    }),
+    //posts: relationship({ ref: 'Post.author', many: true }),
     wallet: text({
       label: '區塊鏈錢包',
+    }),
+    user: relationship({
+      label: 'User',
+      ref: 'User.publisher',
+      many: true,
     }),
   },
   ui: {
@@ -99,6 +91,5 @@ const listConfigurations = list ({
     },
   },
 })
-
 
 export default utils.addTrackingFields(listConfigurations)
