751d98318405007611f3b61232726d506b1c8c53
nickchen111 <s27206383@gmail.com>
2025-01-03 15:33:45 +0800

    feat: modify report record column for adding label and add checking code

--- packages/mesh/lists/report_record.ts
+++ packages/mesh/lists/report_record.ts
@@ -4,13 +4,50 @@ import { text, relationship,} from '@keystone-6/core/fields'
 
 const { allowRoles, admin, moderator, editor } = utils.accessControl
 
+const validateRequiredFields = (
+    fields: string[],
+    resolvedData: Record<string, any>,
+    addValidationError: (msg: string) => void
+  ): void => {
+    fields.forEach((field) => {
+      if (!resolvedData[field]) {
+        addValidationError(`The "${field}" field is required.`);
+      }
+    });
+  };
+
 const listConfigurations = list({
     fields: {
-      informant: relationship({ ref: 'Member', many: false }),
-      reason: relationship({ ref: 'ReportReason', many: false }),
-      respondent: relationship({ ref: 'Member', many: false }),
-      comment: relationship({ ref: 'Comment', many: false }),
-      collection: relationship({ ref: 'Collection', many: false }),
+      informant: relationship({ 
+            ref: 'Member', 
+            many: false, 
+            label: "發起人",
+        }),
+      reason: relationship({ 
+            ref: 'ReportReason', 
+            many: false, 
+            label:'檢舉原因' 
+        }),
+      respondent: relationship({ 
+            ref: 'Member',
+            many: false,
+            label:'被檢舉人', 
+        }),
+      comment: relationship({ 
+            ref: 'Comment', 
+            many: false,
+            label:'被檢舉留言',
+        }),
+      collection: relationship({ 
+            ref: 'Collection', 
+            many: false,
+            label:'被檢舉集錦',
+        }),
+    },
+    hooks: {
+        validateInput: ({ resolvedData, addValidationError }) => {
+          validateRequiredFields(['informant', 'reason', 'respondent'], resolvedData, addValidationError);
+        },
     },
     ui: {
       listView: {
