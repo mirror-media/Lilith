d793cb74f2b85c55f1d4ef93750b4a570dbb3a0d
nickchen111 <s27206383@gmail.com>
2025-02-13 17:56:05 +0800

    feat : add column podcast_url in publisher list

--- packages/mesh/migrations/20250213095413_add_column_podcast_url_in_publisher_list/migration.sql
+++ packages/mesh/migrations/20250213095413_add_column_podcast_url_in_publisher_list/migration.sql
@@ -4,5 +4,8 @@
   - A unique constraint covering the columns `[customId]` on the table `Publisher` will be added. If there are existing duplicate values, this will fail.
 
 */
+-- AlterTable
+ALTER TABLE "Publisher" ADD COLUMN     "podcast_url" TEXT NOT NULL DEFAULT E'';
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Publisher_customId_key" ON "Publisher"("customId");
