7794ec3b8368493f51ea1105c2314b395eba91e5
Tsuki Akiba <erase2004@gmail.com>
2025-02-26 15:36:49 +0800

    fix(daily): add request content-type

--- packages/mirrordaily/utils/invalidate-cdn-cache.ts
+++ packages/mirrordaily/utils/invalidate-cdn-cache.ts
@@ -18,16 +18,24 @@ function checkURLIsDesignated() {
 }
 
 /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+function requestAPI(url: string, data: any) {
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+}
+
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 export async function invalidateStoryCache(slug: any) {
   if (checkURLIsDesignated() === false) return
 
   const url = `${invalidateCDNCacheServerURL}/story`
 
   try {
-    await fetch(url, {
-      method: 'POST',
-      body: JSON.stringify({ slug }),
-    })
+    await requestAPI(url, { slug })
   } catch (err) {
     const payload = { slug, error: {} }
 
