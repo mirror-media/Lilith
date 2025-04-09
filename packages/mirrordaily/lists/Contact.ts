a3d2bcfb0b143da589a6169b5b5100ee1e1ec2f1
Tsuki Akiba <erase2004@gmail.com>
2025-04-09 12:41:38 +0800

    refactor(daily): move sections from User to Contact

--- packages/mirrordaily/lists/Contact.ts
+++ packages/mirrordaily/lists/Contact.ts
@@ -1,8 +1,9 @@
 import { utils } from '@mirrormedia/lilith-core'
 import { list } from '@keystone-6/core'
-import { text } from '@keystone-6/core/fields'
+import { relationship, text } from '@keystone-6/core/fields'
 
-const { allowRoles, admin, moderator, editor } = utils.accessControl
+const { allowRoles, admin, moderator, editor, contributor } =
+  utils.accessControl
 
 const listConfigurations = list({
   fields: {
@@ -15,18 +16,26 @@ const listConfigurations = list({
       label: '敘述',
       ui: { displayMode: 'textarea' },
     }),
+    sections: relationship({
+      label: '大分類',
+      ref: 'Section',
+      many: true,
+      ui: {
+        labelField: 'name',
+      },
+    }),
   },
   ui: {
     labelField: 'name',
     listView: {
-      initialColumns: ['id', 'name', 'createdAt'],
+      initialColumns: ['id', 'name', 'sections', 'createdAt'],
       initialSort: { field: 'id', direction: 'DESC' },
       pageSize: 50,
     },
   },
   access: {
     operation: {
-      query: allowRoles(admin, moderator, editor),
+      query: allowRoles(admin, moderator, editor, contributor),
       update: allowRoles(admin, moderator, editor),
       create: allowRoles(admin, moderator, editor),
       delete: allowRoles(admin),
