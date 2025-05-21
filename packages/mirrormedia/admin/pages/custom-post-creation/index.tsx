d61cfcb955311a92bb42d060cd5d0aac2a119623
hcchien <hcchien@gmail.com>
2025-05-21 09:53:10 +0800

    feat(mirrormedia): add post creating page for mobile

---
+++ packages/mirrormedia/admin/pages/custom-post-creation/index.tsx
@@ -0,0 +1,109 @@
+/** Reference: @keystone-6/core@5.2.0:packages\core\src\___internal-do-not-use-will-break-in-patch\admin-ui\pages\CreateItemPage\index.tsx */
+
+import type { ListMeta } from '@keystone-6/core/types'
+import { pick } from 'lodash-es'
+import { useList, useKeystone } from '@keystone-6/core/admin-ui/context'
+import {
+  PageContainer,
+  GraphQLErrorNotice,
+} from '@keystone-6/core/admin-ui/components'
+import { Fields } from '@keystone-6/core/admin-ui/utils'
+import { useRouter } from '@keystone-6/core/admin-ui/router'
+
+import { Box } from '@keystone-ui/core'
+import { LoadingDots } from '@keystone-ui/loading'
+import { Button } from '@keystone-ui/button'
+import {
+  ColumnLayout,
+  ItemPageHeader,
+  BaseToolbar,
+} from '../../components/common'
+import { useCreateItem } from '../../utils/use-create-item'
+
+const LIST_KEY = 'Post'
+const PICKED_FIELDS = [
+  'id',
+  'slug',
+  'title',
+  'sections',
+  'writers',
+  'content',
+  'heroImage',
+  'heroCaption',
+  'defaultHeroImage',
+  'relateds',
+  'state',
+  'publishedDate',
+]
+
+function CreatePageForm(props: { list: ListMeta }) {
+  const createItem = useCreateItem(props.list)
+  const router = useRouter()
+
+  return (
+    <Box paddingTop="xlarge">
+      {createItem.error && (
+        <GraphQLErrorNotice
+          networkError={createItem.error?.networkError}
+          errors={createItem.error?.graphQLErrors}
+        />
+      )}
+
+      <Fields {...createItem.props} />
+      <BaseToolbar>
+        <Button
+          isLoading={createItem.state === 'loading'}
+          weight="bold"
+          tone="active"
+          onClick={async () => {
+            const item = await createItem.create()
+            if (item) {
+              router.push(`/${props.list.path}/${item.id}`)
+            }
+          }}
+        >
+          Create {props.list.singular}
+        </Button>
+      </BaseToolbar>
+    </Box>
+  )
+}
+
+export default function CustomPostCreation() {
+  const list = useList(LIST_KEY)
+  const { createViewFieldModes } = useKeystone()
+
+  const newList = Object.assign({}, list, {
+    fields: pick(list.fields, PICKED_FIELDS),
+  })
+
+  return (
+    <PageContainer
+      title="Custom Post Creation"
+      header={<ItemPageHeader list={newList} label="Create" />}
+    >
+      <ColumnLayout>
+        <Box>
+          {createViewFieldModes.state === 'error' && (
+            <GraphQLErrorNotice
+              networkError={
+                createViewFieldModes.error instanceof Error
+                  ? createViewFieldModes.error
+                  : undefined
+              }
+              errors={
+                createViewFieldModes.error instanceof Error
+                  ? undefined
+                  : createViewFieldModes.error
+              }
+            />
+          )}
+          {createViewFieldModes.state === 'loading' && (
+            <LoadingDots label="Loading create form" />
+          )}
+          <CreatePageForm list={newList} />
+        </Box>
+      </ColumnLayout>
+    </PageContainer>
+  )
+}
