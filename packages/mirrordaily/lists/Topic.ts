e2cf1e77bbba5a734cbb7cefcfd71f9d4abee221
hcchien <hcchien@gmail.com>
2025-07-11 12:05:38 +0800

    fix: skip the topic json generator

--- packages/mirrordaily/lists/Topic.ts
+++ packages/mirrordaily/lists/Topic.ts
@@ -300,6 +300,7 @@ const listConfigurations = list({
       }
       return resolvedData
     },
+    /*
     afterOperation: async ({ operation, item, context, resolvedData }) => {
 	  if (item?.id && item.state == 'published') {
         const result = fetch(envVar.topicServiceApi + '?topic_id=' + item.id, {
@@ -307,6 +308,7 @@ const listConfigurations = list({
         })
 	  }
 	},
+*/
   },
 })
 
