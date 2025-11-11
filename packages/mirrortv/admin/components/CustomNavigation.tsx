bf0588520e969501ad157724d481b106d4b8124f
chrispclai <chrispclai@mirrormedia.mg>
2025-11-11 17:47:31 +0800

    feat: establish mirrortv framework

---
+++ packages/mirrortv/admin/components/CustomNavigation.tsx
@@ -0,0 +1,37 @@
+import {
+  NavigationContainer,
+  ListNavItem,
+  NavItem,
+} from '@keystone-6/core/admin-ui/components'
+import type { NavigationProps } from '@keystone-6/core/admin-ui/components'
+import { PlusIcon } from '@keystone-ui/icons/icons/PlusIcon'
+
+import { Fragment } from 'react'
+export function CustomNavigation({
+  lists,
+  authenticatedItem,
+}: NavigationProps) {
+  return (
+    <NavigationContainer authenticatedItem={authenticatedItem}>
+      <NavItem href="/">Dashboard</NavItem>
+      {/* creat post shortcut */}
+      <NavItem key="create-post-shortcut" href="/custom-post-creation">
+        Create Post <PlusIcon size="smallish" color="blue" />
+      </NavItem>
+      {lists.map((list) => {
+        const key = list.key
+
+        if (key === 'Photo') {
+          return (
+            <Fragment key={key}>
+              <ListNavItem key={key} list={list} />
+              <NavItem key="multi-images" href="/multi-images">
+                Multi Images
+              </NavItem>
+            </Fragment>
+          )
+        } else return <ListNavItem key={key} list={list} />
+      })}
+    </NavigationContainer>
+  )
+}
---
+++ packages/mirrortv/environment-variables.ts
@@ -0,0 +1,97 @@
+import { ACL } from './type'
+
+// 確認環境變數
+const {
+  IS_UI_DISABLED,
+  ACCESS_CONTROL_STRATEGY,
+  PREVIEW_SERVER_ORIGIN,
+  PREVIEW_SERVER_PATH,
+  DATABASE_PROVIDER,
+  DATABASE_URL,
+  SESSION_SECRET,
+  SESSION_MAX_AGE,
+  GCS_BUCKET,
+  FILES_BASE_URL,
+  FILES_STORAGE_PATH,
+  IMAGES_BASE_URL,
+  IMAGES_STORAGE_PATH,
+  VIDEOS_BASE_URL,
+  VIDEOS_STORAGE_PATH,
+  LOCK_DURATION,
+  IS_CACHE_ENABLED,
+  REDIS_SERVER,
+  CACHE_IDENTIFIER,
+  CACHE_CONNECT_TIMEOUT,
+  CACHE_MAXAGE,
+  DATA_SERVICE_API,
+  TOPIC_SERVICE_API,
+  AUTO_TAGGING,
+  INVALID_CDN_CACHE_SERVER_URL,
+  YOUTUBE_API_KEY,
+} = process.env
+
+enum DatabaseProvider {
+  Sqlite = 'sqlite',
+  Postgres = 'postgresql',
+}
+
+const cacheMaxAge = Number(CACHE_MAXAGE)
+const cacheConnectTimeout = Number(CACHE_CONNECT_TIMEOUT)
+
+export default {
+  isUIDisabled: IS_UI_DISABLED === 'true',
+  accessControlStrategy: ACCESS_CONTROL_STRATEGY || ACL.CMS, // the value could be one of 'cms', 'gql' or 'preview'
+  previewServer: {
+    origin: PREVIEW_SERVER_ORIGIN || 'http://localhost:3001',
+    path: PREVIEW_SERVER_PATH || '/preview-server',
+  },
+  database: {
+    provider:
+      DATABASE_PROVIDER === 'sqlite'
+        ? DatabaseProvider.Sqlite
+        : DatabaseProvider.Postgres,
+    url:
+      DATABASE_URL || 'postgres://username:password@localhost:5432/mirrordaily',
+  },
+  session: {
+    secret:
+      SESSION_SECRET ||
+      'default_session_secret_and_it_should_be_more_than_32_characters',
+    maxAge:
+      (typeof SESSION_MAX_AGE === 'string' && parseInt(SESSION_MAX_AGE)) ||
+      60 * 60 * 24 * 1, // 1 days
+  },
+  gcs: {
+    bucket: GCS_BUCKET || 'static-vision-tw-dev',
+  },
+  files: {
+    baseUrl: FILES_BASE_URL || '/files',
+    storagePath: FILES_STORAGE_PATH || 'public/files',
+  },
+  images: {
+    baseUrl: IMAGES_BASE_URL || '/images',
+    storagePath: IMAGES_STORAGE_PATH || 'public/images',
+  },
+  videos: {
+    baseUrl: VIDEOS_BASE_URL || '/video-files',
+    storagePath: VIDEOS_STORAGE_PATH || 'public/video-files',
+  },
+  lockDuration:
+    (typeof LOCK_DURATION === 'string' && parseInt(LOCK_DURATION)) || 30,
+  cache: {
+    isEnabled: IS_CACHE_ENABLED === 'true',
+    identifier: CACHE_IDENTIFIER ?? 'weekly-cms',
+    url: REDIS_SERVER ?? '',
+    connectTimeOut: Number.isNaN(cacheConnectTimeout)
+      ? 1000 * 10
+      : cacheConnectTimeout, // unit: millisecond
+    maxAge: Number.isNaN(cacheMaxAge) ? 60 : cacheMaxAge, // unit: second
+  },
+  dataServiceApi: DATA_SERVICE_API,
+  topicServiceApi: TOPIC_SERVICE_API,
+  autotagging: AUTO_TAGGING || 'false',
+  invalidateCDNCacheServerURL: INVALID_CDN_CACHE_SERVER_URL,
+  youtube: {
+    apiKey: YOUTUBE_API_KEY,
+  },
+}
