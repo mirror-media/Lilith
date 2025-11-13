7e048fa537d6e1b3176744e3adbeb6b274d1bbec
rice <rice9547@gmail.com>
2025-11-13 16:48:57 +0800

    fix(advertise): remove length validation from ad name field

--- packages/advertise/lists/Order.ts
+++ packages/advertise/lists/Order.ts
@@ -143,9 +143,6 @@ const listConfigurations = list({
       label: '廣告名稱',
       validation: {
         isRequired: true,
-        length: {
-          max: 10,
-        },
       },
     }),
     nameEditable: checkbox({
