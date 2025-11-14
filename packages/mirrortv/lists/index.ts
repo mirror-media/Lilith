2039b4e3bda13dd4153d285ef8ff05c746f977cb
chrispclai <chrispclai@mirrormedia.mg>
2025-11-14 16:50:49 +0800

    feat: add Partner list with tracking

--- packages/mirrortv/lists/index.ts
+++ packages/mirrortv/lists/index.ts
@@ -1,7 +1,9 @@
+import Partner from './Partner'
 import User from './User'
 import { utils } from '@mirrormedia/lilith-core'
 
 const { addTrackingFields } = utils
 export const listDefinition = {
   User: addTrackingFields(User),
+  Partner,
 }
